b5537b287534fa96c1b0ec37fff9a97014918be9
muyang <muyang@gaoding.com>
2021-01-07 20:08:54 +0800

    kotlin test

---
+++ app/src/main/java/com/roobo/baselibiray/A.kt
@@ -0,0 +1,12 @@
+package com.roobo.baselibiray
+
+
+fun test(any: Any?) {
+    val string = any as? String
+    string?.length;
+}
+
+fun test1(any: Any?) {
+    val string = any as? String
+    string?.length;
+}
